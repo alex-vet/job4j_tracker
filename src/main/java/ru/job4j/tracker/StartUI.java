895314a81cd68b44cd5abe295deb29abbdb68a91
alex-vet <alex157@inbox.ru>
2020-12-08 21:39:19 +0400

    7. toString [#383382]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
-        Item itm = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(itm.getCreated().format(formatter));
+        Item itm = new Item(1,"test");
+        System.out.println(itm);
     }
 }
