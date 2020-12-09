289c937f13568bff56ef4b67d8ae0576d8e70e58
alex-vet <alex157@inbox.ru>
2020-12-09 20:39:17 +0400

    5. Tracker - хранилище [#383394]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,9 @@ package ru.job4j.tracker;
 public class StartUI {
     public static void main(String[] args) {
         Item itm = new Item(1, "test");
+        Tracker trk = new Tracker();
+        trk.add(itm);
+        itm = trk.findById(1);
         System.out.println(itm);
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,28 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item findByName(String name) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (name.equals(item.getName())) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        int newSize = 0;
+        Item[] rsl = new Item[size];
+        for (Item itm : items) {
+            if (itm != null) {
+                rsl[newSize] = itm;
+            }
+        }
+        return Arrays.copyOf(rsl,newSize);
     }
 }
