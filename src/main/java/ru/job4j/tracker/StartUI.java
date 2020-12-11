bc78f729c3ff1d6321fbbf6d87de7e908a5b0621
alex-vet <alex157@inbox.ru>
2020-12-11 20:40:42 +0400

    2.1. Реализация класса StartUI [#383326]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,89 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
+
 public class StartUI {
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+            } else if (select == 1) {
+                System.out.println("=== Show all Items ====");
+                Item[] items = tracker.findAll();
+                for (Item itm : items) {
+                    System.out.println(itm.toString());
+                }
+            } else if (select == 2) {
+                System.out.println("=== Edit Item ====");
+                System.out.print("Enter id Item for replace: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name for new Item: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Item was added.");
+                } else {
+                    System.out.println("Cannot add Item.");
+                }
+            } else if (select == 3) {
+                System.out.println("=== Delete Item ====");
+                System.out.print("Enter id Item for delete: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("Item was deleted.");
+                } else {
+                    System.out.println("Cannot delete Item.");
+                }
+            } else if (select == 4) {
+                System.out.println("=== Find Item by Id ====");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item itm = tracker.findById(id);
+                if (itm != null) {
+                    System.out.println(itm.toString());
+                } else {
+                    System.out.println("Item with id=" + id + " not found.");
+                }
+            } else if (select == 5) {
+                System.out.println("=== Find Items by Name ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item itm : items) {
+                        System.out.println(itm.toString());
+                    }
+                } else {
+                    System.out.println("Items with name=" + name + " not found.");
+                }
+            } else if (select == 6) {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("Menu.");
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
+    }
+
     public static void main(String[] args) {
-        Item itm = new Item(1, "test");
-        Tracker trk = new Tracker();
-        trk.add(itm);
-        itm = trk.findById(1);
-        System.out.println(itm);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
