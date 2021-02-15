4121f939aee10f7f78b881e0c1720c2b2d31f7c4
alex-vet <alex157@inbox.ru>
2021-02-15 05:26:38 +0400

    0. Map, HashMap [#383301]

---
+++ src/main/java/ru/job4j/collection/UsaageMap.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsaageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("ivanov@yandex.ru", "Ivan Ivanov");
+        for (String key : map.keySet()) {
+            System.out.println(key + " = " + map.get(key));
+        }
+    }
+}
