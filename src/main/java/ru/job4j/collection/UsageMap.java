5aa0175ae27775b00b3cdaa4061cdbdc4b969b5f
alex-vet <alex157@inbox.ru>
2021-02-15 05:30:43 +0400

    refactor class name

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -2,7 +2,7 @@ package ru.job4j.collection;
 
 import java.util.HashMap;
 
-public class UsaageMap {
+public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("ivanov@yandex.ru", "Ivan Ivanov");
